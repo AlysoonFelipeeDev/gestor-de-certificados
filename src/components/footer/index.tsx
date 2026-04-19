0b8995580d18803e25699b5990eb6a23f4653380
jean <alysoonfelipe@hotmail.com>
2026-04-19 20:18:26 -0300

    feat: adiciona tag Link para navegação

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,17 +2,8 @@
 
 import { usePathname } from "next/navigation";
 import styles from "./index.module.scss";
-
-interface Route {
-    label: string,
-    route: string
-}
-
-const routes: Route[] = [
-    {label: 'Rota', route: '/rota'},
-    {label: 'Painel', route: '/'},
-    {label: 'Config', route: '/config'} 
-]
+import Link from "next/link";
+import { routes } from "@/constants/routes";
 
 export function Footer() {
     const currentRoute = usePathname()
@@ -20,17 +11,16 @@ export function Footer() {
     return (
             <footer className={styles['bottom-nav']} aria-label="Navegação principal">
                 {routes.map(route => (
-                    <button key={route.label} 
+                    <Link href={route.route}
+                        key={route.label} 
                         className={
                             currentRoute === route.route 
                             ? `${styles['bottom-nav__item']} ${styles['bottom-nav__item--active']}` 
                             : styles['bottom-nav__item'] 
                         }
-                        type="button"
                         >
-                        
                         {route.label}
-                    </button>
+                    </Link>
                 ))}
             </footer>
         )
