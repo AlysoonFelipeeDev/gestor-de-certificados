ee8718d562d22d7be93349f1bf329333bd1153e4
jean <alysoonfelipe@hotmail.com>
2026-04-19 02:38:02 -0300

    feat: criado header e sua estilização

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,16 @@
+import styles from "./index.module.scss";
+
 export function Header() {
-    
+    return (
+        <header className={styles.topbar}>
+            <button className={styles['topbar__icon-button']} type="button" aria-label="Abrir menu">
+                ☰
+            </button>
+            <h1 className={styles.topbar__title}>Dashboard</h1>
+            <button className={styles['topbar__icon-button']} type="button" aria-label="Notificações">
+                🔔
+            </button>
+        </header>
+    )
 }
+
