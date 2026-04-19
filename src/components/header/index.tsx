d7d66f87593159a5d97c1ad3510c11a8481c8ced
jean <alysoonfelipe@hotmail.com>
2026-04-19 20:20:16 -0300

    feat: header dinâmico com base na rota atual

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,22 @@
+'use client';
+import { usePathname } from "next/navigation";
 import styles from "./index.module.scss";
+import { routes } from "@/constants/routes";
+import Link from "next/link";
+
 
 export function Header() {
+    const pathName =  usePathname()
+    const currentPage = routes.find(rout => rout.route === pathName)
+    const isHome = pathName === '/'
+
     return (
         <header className={styles.topbar}>
-            <button className={styles['topbar__icon-button']} type="button" aria-label="Abrir menu">
-                ☰
-            </button>
-            <h1 className={styles.topbar__title}>Dashboard</h1>
+            {isHome 
+                ? <button className={styles['topbar__icon-button']} type="button" aria-label="Abrir menu">☰</button> 
+                : <Link href={'/'}className={styles['topbar__icon-button']} aria-label="Voltar para Painel">⬅</Link>
+            }
+            <h1 className={styles.topbar__title}>{currentPage?.label ?? 'Página não encontrada'}</h1>
             <button className={styles['topbar__icon-button']} type="button" aria-label="Notificações">
                 🔔
             </button>
