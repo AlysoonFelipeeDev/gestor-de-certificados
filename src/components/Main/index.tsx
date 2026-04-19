415a1d1be6e712a68b7ba065ba7cb39f4965e271
jean <alysoonfelipe@hotmail.com>
2026-04-19 02:46:45 -0300

    feat: container das páginas

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,15 @@
-export function Main () {
+import styles from "./index.module.scss";
 
+interface MainPops {
+    children: React.ReactNode
+}
+
+export function Main ({children}: MainPops) {
+    return (
+        <main className={styles['app-layout']}>
+            <section className={styles['app-section']}>
+                {children}
+            </section>
+        </main>
+    )
 }
