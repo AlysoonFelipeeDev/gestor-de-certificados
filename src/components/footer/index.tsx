4478668d6d8fb4b15401b6cbf334b672e24c6398
jean <alysoonfelipe@hotmail.com>
2026-04-19 02:36:58 -0300

    criado componente footer e sua estilização

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,3 +1,37 @@
+'use client'
+
+import { usePathname } from "next/navigation";
+import styles from "./index.module.scss";
+
+interface Route {
+    label: string,
+    route: string
+}
+
+const routes: Route[] = [
+    {label: 'Rota', route: '/rota'},
+    {label: 'Painel', route: '/'},
+    {label: 'Config', route: '/config'} 
+]
+
 export function Footer() {
-    
+    const currentRoute = usePathname()
+
+    return (
+            <footer className={styles['bottom-nav']} aria-label="Navegação principal">
+                {routes.map(route => (
+                    <button key={route.label} 
+                        className={
+                            currentRoute === route.route 
+                            ? `${styles['bottom-nav__item']} ${styles['bottom-nav__item--active']}` 
+                            : styles['bottom-nav__item'] 
+                        }
+                        type="button"
+                        >
+                        
+                        {route.label}
+                    </button>
+                ))}
+            </footer>
+        )
 }
