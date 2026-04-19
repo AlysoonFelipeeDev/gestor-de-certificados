44da65bfaaac5d4aabf318e4f20ef4a0a6107df9
jean <alysoonfelipe@hotmail.com>
2026-04-19 20:13:52 -0300

    feat: usar array de rotas globalmente

---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+import { Route } from "@/types/routes";
+
+export const routes: Route[] = [
+    {label: 'Rota Ativa', route: '/rota'},
+    {label: 'Painel', route: '/'},
+    {label: 'Config', route: '/config'} 
+]
