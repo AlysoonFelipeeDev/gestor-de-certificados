1bee909987c062d2623b4a7745cfb605fcab7e47
jean <alysoonfelipe@hotmail.com>
2026-04-19 21:47:04 -0300

    feat: implementa interfaces relaciona a coleta de criação de rota

--- src/types/routes.ts
+++ src/types/routes.ts
@@ -1,4 +1,19 @@
-export interface Route {
+export interface NavigationRoute {
     label: string,
     route: string
+};
+
+export interface CollectionRoute {
+    id: string,
+    establishments: establishmentVisit[],
+    certificateValueWeek: number,
+    registeredRoute: RegisteredRoute,
+    date: Date
+}
+
+export interface RegisteredRoute {
+    id: string,
+    nameRoute: string,
+    employee: string,
+    establishments: Establishment[]
 }
